770ef10adf289daadbdc3fe021f3f11b81abe0e1
Peter Wang <pwang@continuum.io>
2021-01-04 00:34:37 -0600

    Adding helper function to plot satellite tracks

--- Scripts/plot_helpers.py
+++ Scripts/plot_helpers.py
@@ -1,6 +1,7 @@
 """ plot_helpers - functions to help plot things 
 
 """
+import numpy as np
 import pandas as pd
 import holoviews as hv
 from colorcet import fire
@@ -20,6 +21,24 @@ def plot_points(df, width=700, height=500):
     return tiles * r.opts(cmap=fire[180:], width=width, height=width,
             cnorm='eq_hist', alpha=0.5)
 
+
+def plot_satellite(lats, lons, lat_clip=85.5):
+    """ Returns a holoviews Curve that shows a satellite track. 
+    Longitudes are assumed to be between (-180,180), i.e. no correction is done on them.
+    """
+    lats = lats.to_numpy()
+    lons = lons.to_numpy()
+    mask = np.abs(lats) > lat_clip
+    lats[mask] = np.nan
+    lons[mask] = np.nan
+
+    eastings, northings = hv.util.transform.lon_lat_to_easting_northing(lons,lats)
+    # Heuristic to insert NaNs to break up Curve (prevent wrapping issues at date line)
+    inds = np.where(np.abs(np.diff(eastings)) > 2e7)[0] # Big delta to split on
+    inds += 1
+    eastings  = np.insert(eastings,  inds, [np.nan for i in range(len(inds))])
+    northings = np.insert(northings, inds, [np.nan for i in range(len(inds))])
+    return hv.Curve((eastings, northings)).opts(color='red')
 
 def plot_visibility(hits, mmsi):
     """ Render a histogram plot of all the times that a vessel was visible
