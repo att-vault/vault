81872bf6dc9d23acef5caf7109a75b3a818d78dd
Matty G <meawoppl@gmail.com>
2021-01-07 11:23:49 -0800

    Conda Forge Bits -> Parallel Script (#29)

--- scripts/build_index_parallel.sh
+++ scripts/build_index_parallel.sh
@@ -1,1 +1,6 @@
+# Install gnu-parallel
+conda config --add channels conda-forge
+conda install parallel
+
+# Run the parallel index build
 parallel -a norad_id_active.txt python sathelpers.py --noradID {}
